5160ffca50619caef17d184b859e27b780b44472
jonzeeff@gmail.com <jon@zeeff.com>
2012-05-21 14:30:01 -0400

    comments

--- o5e/src/Engine_OPS.c
+++ o5e/src/Engine_OPS.c
@@ -1,5 +1,5 @@
 /*********************************************************************************
- 
+
     @file      Engine_OPS.c                                                              
     @date      December, 2011
     @brief     Open5xxxECU - this file contains functions for fuel pulse width 
