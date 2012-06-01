c1cd343d3c8fbcd3f06da5b48193cda51cad9bb2
mk e <mark@gemellocattivo.com>
2012-06-01 11:07:22 -0400

    more user variable work
    Signed-off-by: mk e <mark@gemellocattivo.com>

--- o5e/src/eTPU_OPS.c
+++ o5e/src/eTPU_OPS.c
@@ -116,7 +116,7 @@ struct etpu_config_t my_etpu_config = {
 int32_t init_eTPU()
 {
     uint32_t i;
-    uint24_t Cam_Window_Width;        // this mght want to move to the tuner - sets the cam window ± 15 degrees from expected location
+    uint24_t Cam_Window_Width;
     uint24_t Engine_Position_eTPU;
     uint24_t Cam_Window_Open;
     uint24_t Cam_Lobe_Pos_eTPU;
@@ -140,7 +140,7 @@ int32_t init_eTPU()
     Engine_Position_eTPU = (72000 - ((uint32_t)Engine_Position << 2));   // adjust bin -2 to bin 0
     Cam_Lobe_Pos_eTPU = (72000 -  ((uint32_t)Cam_Lobe_Pos << 2)) ;      // adjust bin -2 to bin 0
     Cam_Window_Open = (72000 + Cam_Lobe_Pos_eTPU - (Cam_Window_Open_Set << 2) ) % 72000; //adjust bin -2 to bin 0
-    Cam_Window_Width = Cam_Window_Width_Set;
+    Cam_Window_Width = Cam_Window_Width_Set <<2; //adjust bin -2 to bin 0
     // set the cam window correctly for semi-sequentail mode
     if Sync_Mode_Select {
        Cam_Window_Open = 36000;
