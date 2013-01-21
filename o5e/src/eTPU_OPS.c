bc13113ae3789873dfd5a19c4ffb27156a0242aa
mk_e <mark@gemellocattivo.com>
2013-01-21 16:00:44 -0500

    more work on the crank windowing values
    Signed-off-by: mk_e <mark@gemellocattivo.com>

--- o5e/src/eTPU_OPS.c
+++ o5e/src/eTPU_OPS.c
@@ -160,18 +160,12 @@ int32_t init_eTPU()
 
     // cam window starts 1/2 of the window before the cam signal
     int32_t Window_Start;
-    int32_t crank_windowing_ratio_normal = 0x7fffff;
-    int32_t crank_windowing_ratio_after_gap;
-    int32_t crank_windowing_ratio_across_gap;
-    int32_t crank_windowing_ratio_timeout;
-    int32_t crank_gap_ratio = 0xbfffff;
+    int32_t crank_gap_ratio = 0xafffff;
     
-    Window_Start = ((int32_t)Cam_Lobe_Pos << 2) - (((int32_t)Cam_Window_Width_Set << 2) / 2);       /* cam_angle_window_start  */
-    crank_gap_ratio = crank_gap_ratio * 2 / (1+ Missing_Teeth);
-    crank_windowing_ratio_after_gap = crank_windowing_ratio_normal * 2 / (1+ Missing_Teeth);
-    crank_windowing_ratio_across_gap = crank_windowing_ratio_after_gap;
-    crank_windowing_ratio_timeout = crank_windowing_ratio_normal;
-    
+    Window_Start = ((int32_t)Cam_Lobe_Pos << 2) - (((int32_t)Cam_Window_Width_Set << 2) / 2);/* cam_angle_window_start  */
+    if (Missing_Teeth > 1) {
+    	crank_gap_ratio = 0xffffff;
+    }    
 
     #define Ticks_Per_Tooth  120        // Max 200
 
@@ -207,10 +201,10 @@ int32_t init_eTPU()
                                         Missing_Teeth,                  /* crank_number_of_missing_teeth */
                                         Total_Teeth/3,                  /* crank_blank_tooth_count */
                                         Ticks_Per_Tooth,                /* crank_tcr2_ticks_per_tooth */
-                                        crank_windowing_ratio_normal,   /* crank_windowing_ratio_normal: 0x199999 */
-                                        crank_windowing_ratio_after_gap,/* crank_windowing_ratio_after_gap: 0x199999 */
-                                        crank_windowing_ratio_across_gap,/* crank_windowing_ratio_across_gap: 0x199999 */
-                                        crank_windowing_ratio_timeout,  /* crank_windowing_ratio_timeout: 0x299999 */
+                                        0xffffff,                       /* crank_windowing_ratio_normal: 0x199999 */
+                                        0xffffff,                       /* crank_windowing_ratio_after_gap: 0x199999 */
+                                        0xffffff,                       /* crank_windowing_ratio_across_gap: 0x199999 */
+                                        0xffffff,  /* crank_windowing_ratio_timeout: 0x299999 */
                                         crank_gap_ratio,                /* crank_gap_ratio: 0x9fffff */
                                         5,                              /* crank_blank_time_ms */
                                         700000/Total_Teeth,             /* crank_first_tooth_timeout_us */
