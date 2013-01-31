ec6d4a57f9e0b5faf57a4bab3d3b3a25f85cc585
mk_e <mark@gemellocattivo.com>
2013-01-31 15:38:27 -0500

    moved pins to match Jared's I/O board....still question's on AN pins
    Signed-off-by: mk_e <mark@gemellocattivo.com>

--- o5e/headers/config.h
+++ o5e/headers/config.h
@@ -10,28 +10,28 @@
 // TODO define eTPU channels to use here
 // Note: different hardware may have to change these
 // Note: what is here must match what is in SIU_OPS.c
-#define FUEL_CHANNELS_1_6     21,9,10,11,12,13   //5 used instead of 10 to get LED on on deme board
-#define FUEL_CHANNELS_7_12    18,20,22,23,24,25
-#define FUEL_CHANNELS_13_18   25,25,25,25,25,25  /* not used for 5634 */
-#define FUEL_CHANNELS_19_24   25,25,25,25,25,25  /* not used for 5634*/
+#define FUEL_CHANNELS_1_6     25,24,22,21,8,9   //5 used instead of 10 to get LED on on deme board
+#define FUEL_CHANNELS_7_12    12,14,5,15,20,20
+#define FUEL_CHANNELS_13_18   20,20,20,20,20,20  /* not used for 5634 */
+#define FUEL_CHANNELS_19_24   20,20,20,20,20,20  /* not used for 5634*/
 //changed the channel 18 for testing
-#define SPARK_CHANNELS_1_6    19,2,3,4,10,6        // 10 used instead of 5,to give LED to fuel 1
-#define SPARK_CHANNELS_7_12   7,8,25,25,25,25   /* 9-12 not used for 5634*/ 
-#define TACH_CHANNEL          27                  // Tach Output (pwm) eTPU chan
-#define FUEL_PUMP_PIN         141                 // fuel pump
-#define WHEEL_SPEED_1_4       14, 15, 16, 17      // read wheel speed
+#define SPARK_CHANNELS_1_6    10,7,6,4,3,2        // 10 used instead of 5,to give LED to fuel 1
+#define SPARK_CHANNELS_7_12   20,20,20,20,20,20   /* 9-12 not used for 5634*/ 
+#define TACH_CHANNEL          11                  // Tach Output (pwm) eTPU chan
+#define FUEL_PUMP_PIN         127                 // fuel pump
+#define WHEEL_SPEED_1_4       16,17,18,19      // read wheel speed
 //for testing - Blink based on engine position status
 #define MAP_WINDOW_CHANNEL    26    // eTPU channel to output MAP sample windows on - fixed, do not change
 #define KNOCK_WINDOW_CHANNEL  28    // eTPU channel to output MAP sample windows on - fixed
-#define FAKE_CAM_PIN          193   // GPIO used for semi-sequentail operation
+#define FAKE_CAM_PIN          137   // GPIO used for semi-sequentail operation
 
 // used for serial port A and tuner communications
 #define BAUD_RATE 115200    // speed of serial port comm
 #define SERIAL_BUFFER_SIZE (2048+10)
 
 // servo motor or idle air control
-#define PWM1_CHANNEL 29     // eTPU channel to use for servo motor output 
-#define PWM1_PAD     218    // servo GPIO pad # for above
+#define PWM1_CHANNEL 27     // eTPU channel to use for servo motor output 
+#define PWM1_PAD     143    // servo GPIO pad # for above
 #define PWM1_HZ      50     // recommend 50 for servos, 200 or 300 for IAC
 
 // increasing clock available for general use - runs at CPU_CLOCK/1000000 ticks per msec
--- o5e/src/SIU_OPS.c
+++ o5e/src/SIU_OPS.c
@@ -74,34 +74,34 @@ void init_SIU(void)
         // eTPU0, pin 114 is special and needs a 11 instead of 01 for primary function
         SIU.PCR[114].R = B0011 | INPUT; 	// configure  for CRANK (TCRCLKA or eTPU[0])
         SIU.PCR[115].R = B0001 | INPUT;     // cam sensor input eTPU1
-        SIU.PCR[116].R = B0001 | OUTPUT; 	// eTPU[2] ignition 1
-        SIU.PCR[117].R = B0001 | OUTPUT; 	// eTPU[3] ignition 2
-        SIU.PCR[118].R = B0001 | OUTPUT; 	// eTPU[4] ignition 3
-        SIU.PCR[119].R = B0001 | OUTPUT; 	// eTPU[5] injector 1
-        SIU.PCR[120].R = B0001 | OUTPUT; 	// eTPU[6] ignition 5
-        SIU.PCR[121].R = B0001 | OUTPUT; 	// eTPU[7] ignition 6
-        SIU.PCR[122].R = GPIO  | OUTPUT; 	// eTPU[8] ignition 4
-        SIU.PCR[123].R = GPIO  | OUTPUT; 	// eTPU[9] injector 2
-        SIU.PCR[124].R = B0001 | OUTPUT; 	// eTPU[10] injector 3
-        SIU.PCR[125].R = B0001 | OUTPUT; 	// eTPU[11] injector 4
-        SIU.PCR[126].R = B0001 | OUTPUT; 	// eTPU[12] injector 5
-        SIU.PCR[127].R = B0001 | OUTPUT; 	// eTPU[13] injector 6
-        SIU.PCR[128].R = B0001 | INPUT; 	// eTPU[14] wheelspeed FLinjector 7
-        SIU.PCR[129].R = B0001 | INPUT; 	// eTPU[15] wheelspeed FRpin available 
-        SIU.PCR[130].R = B0001 | INPUT;     // eTPU[16] wheelspeed RLinjector 8
-        SIU.PCR[131].R = B0001 | INPUT;     // eTPU[17] wheelspeed RR
-        SIU.PCR[132].R = B0001 | INPUT;    // eTPU[18]  
-        SIU.PCR[133].R = B0001 | INPUT;    // eTPU[19] 
-        SIU.PCR[134].R = B0001 | INPUT;    // eTPU[20] 
-        SIU.PCR[135].R = GPIO  | UNUSED; 	// eTPU[21] pin available
-        SIU.PCR[136].R = B0001 | OUTPUT; 	// eTPU[22] injector 9
-        SIU.PCR[137].R = B0001 | OUTPUT; 	// eTPU[23] injector 10
-        SIU.PCR[138].R = B0001 | OUTPUT; 	// eTPU[24] injector 11
-        SIU.PCR[139].R = B0001 | OUTPUT; 	// eTPU[25] injector 12 
+        SIU.PCR[116].R = B0001 | OUTPUT; 	// eTPU[2] ignition 6
+        SIU.PCR[117].R = B0001 | OUTPUT; 	// eTPU[3] ignition 5
+        SIU.PCR[118].R = B0001 | OUTPUT; 	// eTPU[4] ignition 4
+        SIU.PCR[119].R = B0001 | OUTPUT; 	// eTPU[5] injector 9
+        SIU.PCR[120].R = B0001 | OUTPUT; 	// eTPU[6] ignition 3
+        SIU.PCR[121].R = B0001 | OUTPUT; 	// eTPU[7] ignition 2
+        SIU.PCR[122].R = GPIO  | OUTPUT; 	// eTPU[8] injector 5
+        SIU.PCR[123].R = GPIO  | OUTPUT; 	// eTPU[9] injector 6
+        SIU.PCR[124].R = B0001 | OUTPUT; 	// eTPU[10] ignition 1
+        SIU.PCR[125].R = B0001 | OUTPUT; 	// eTPU[11] Tach - injector 12
+        SIU.PCR[126].R = B0001 | OUTPUT; 	// eTPU[12] injector 7
+        SIU.PCR[127].R = B0001 | OUTPUT; 	// eTPU[13] fuel pump  - injector 11
+        SIU.PCR[128].R = B0001 | INPUT; 	// eTPU[14] injector 8
+        SIU.PCR[129].R = B0001 | INPUT; 	// eTPU[15] injector 10
+        SIU.PCR[130].R = B0001 | INPUT;     // eTPU[16] wheelspeed FL
+        SIU.PCR[131].R = B0001 | INPUT;     // eTPU[17] wheelspeed FR
+        SIU.PCR[132].R = B0001 | INPUT;    // eTPU[18] wheelspeed RL 
+        SIU.PCR[133].R = B0001 | INPUT;    // eTPU[19] wheelspeed RR
+        SIU.PCR[134].R = B0001 | INPUT;    // eTPU[20] pin available
+        SIU.PCR[135].R = GPIO  | UNUSED; 	// eTPU[21] injector 4
+        SIU.PCR[136].R = B0001 | OUTPUT; 	// eTPU[22] injector 3
+        SIU.PCR[137].R = B0001 | OUTPUT; 	// eTPU[23] fake cam signal
+        SIU.PCR[138].R = B0001 | OUTPUT; 	// eTPU[24] injector 2
+        SIU.PCR[139].R = B0001 | OUTPUT; 	// eTPU[25] injector 1 
         SIU.PCR[140].R = GPIO  | UNUSED; 	// eTPU[26] pin available, eTPU26 used for MAP window internal
-        SIU.PCR[141].R = B0001 | OUTPUT; 	// eTPU[27] Fuel Pump & etpu channel reserved for Tach signal
+        SIU.PCR[141].R = B0001 | OUTPUT; 	// eTPU[27] Stepper PWM signal  Fuel Pump & etpu channel
         SIU.PCR[142].R = GPIO  | UNUSED; 	// eTPU[28] pin available, & Knock window internal
-        SIU.PCR[143].R = B0001 | OUTPUT; 	// eTPU[29] PWM_Pad & PWM signal etpu channel reserved for PWM1 output 
+        SIU.PCR[143].R = B0001 | OUTPUT; 	// eTPU[29] PWM_Pad &  
         SIU.PCR[144].R = B0011 | OUTPUT;    // eTPU[30] toothgen simulator, eTPU30 hardwired to eTPU1 
         SIU.PCR[145].R = B0011 | OUTPUT;    // eTPU[31] toothgen simulator, eTPU31 hardwired to eTPU0 
 
@@ -111,31 +111,31 @@ void init_SIU(void)
         SIU.PCR[181].R = B0001 | OUTPUT; 	// eMIOS 2 DBW2
         SIU.PCR[183].R = B0001 | OUTPUT; 	// eMIOS 4 DBW3
 
-        SIU.PCR[187].R = B0001 | OUTPUT;    // eMIOS 8 DBW4 
-        SIU.PCR[188].R = GPIO | UNUSED;     // eMIOS 9 Pin LED_0 emios 23 used for msec clock
+        SIU.PCR[187].R = B0001 | OUTPUT;    // eMIOS 8
           /* Pins define elsewhere in FS code
+        SIU.PCR[188].R = GPIO | OUTPUT;     // eMIOS 9 Pin LED_0 emios 9 used for msec clock
         SIU.PCR[189].R = GPIO | OUTPUT;     // eMIOS 10 Pin LED_1 and emios 10 is reserved for eQADC trigger
         SIU.PCR[190].R = GPIO | OUTPUT;     // eMIOS 11 Pin LED_2 and emios 11 is reserved for eQADC trigger
         SIU.PCR[191].R = GPIO | OUTPUT;     // eMIOS 12 Pin LED_3 and emios 12 is reserved for eQADC trigger
-        SIU.PCR[192].R = GPIO | OUTPUT; 	// eMIOS no pin on 5634 and emios 13 is reserved for eQADC trigger
-           */
-        SIU.PCR[193].R = GPIO | OUTPUT;     // eMIOS 14 Pin fake cam signal,  and emios 14 is reserved for eQADC trigger
+          */
+           									// eMIOS 13 no pin on 5634 and emios 13 is reserved for eQADC trigger
+        SIU.PCR[193].R = GPIO | OUTPUT;     // eMIOS 14 Pin ,  and emios 14 is reserved for eQADC trigger
         SIU.PCR[194].R = GPIO | OUTPUT; 	// eMIOS no pin on 5634 and emios 15 is reserved for eQADC trigger
 
-        SIU.PCR[202].R = GPIO | UNUSED;;     // eMIOS 23 
+        SIU.PCR[202].R = GPIO | UNUSED;;     // eMIOS 23 DBW4
          
         //AN channel functions (do not require PCR settings)
         //AN0 - Knock1 +
         //AN1 - Knock1 -
-        //AN2 - Knock2 +
-        //AN3 - Knock2 -
+        //AN2 - 
+        //AN3 - 
         //AN9 - MAP_1
-        //AN11 - 
-         SIU.PCR[215].R = ALT2 | OUTPUT; 	//AN12 used for eTPU[19]  ignition 1 
-         SIU.PCR[216].R = ALT2 | OUTPUT; 	//AN13 used for eTPU[21]  injection 1   
-         SIU.PCR[217].R = ALT2 | OUTPUT; 	//AN14 used for eTPU[27] Tach signal  
-         SIU.PCR[218].R = ALT2 | OUTPUT; 	//AN15 used for eTPU[29] PWM1 output               
-        //AN16 - 
+        //AN11 -V_Batt
+         SIU.PCR[215].R = ALT2 | OUTPUT; 	//AN12 eTPU 
+         SIU.PCR[216].R = ALT2 | OUTPUT; 	//AN13    
+         SIU.PCR[217].R = ALT2 | OUTPUT; 	//AN14   
+         SIU.PCR[218].R = ALT2 | OUTPUT; 	//AN15                
+        //AN16 - V_TPS
         //AN17 - POT on dev board
         //AN18 - 
         //AN21 - V_P1
@@ -146,7 +146,7 @@ void init_SIU(void)
         //AN27 - MAP_1
         //AN28 - V_O2_2_UA 
         //AN30 - V_O2_2_UR
-        //AN31 - V_TPS
+        //AN31 - 
         //AN32 - V_P3
         //AN33 - V_P4
         //AN34 - V_P5
